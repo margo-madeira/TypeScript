f49f79bf453b4bd92092a0a2273e562b4a78e9b8
margo-madeira <ligastaeva@mail.ru>
2024-05-03 10:07:27 +0300

    initial commit

---
+++ src/ts/app.ts
@@ -0,0 +1,109 @@
+export default interface Buyable {
+  id: number;
+  name: string;
+  year: number;
+  country: string;
+  tagline: string;
+  genre: string;
+  time: string | number;
+  price: number;
+}
+
+export class Movie implements Buyable {
+  id: number;
+  name: string;
+  year: number;
+  country: string;
+  tagline: string;
+  genre: string;
+  time: string | number;
+  price: number;
+
+  constructor(
+    id: number,
+    name: string,
+    year: number,
+    country: string,
+    tagline: string,
+    genre: string,
+    time: string | number,
+    price: number
+  ) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.country = country;
+    this.tagline = tagline;
+    this.genre = genre;
+    this.time = time;
+    this.price = price;
+  }
+}
+
+export class Cart {
+  //корзина товаров
+  _items: Buyable[] = []; //хранит все товары
+
+  add(item: Buyable): Buyable[] {
+    //добавляет товар в корзину
+    this._items.push(item);
+    return this._items;
+  }
+
+  get items(): Buyable[] {
+    //возвращает список товаров в корзине
+    return [...this._items];
+  }
+
+  totalAmount(): number {
+    //считает итог стоимость всех товаров
+    let total = 0;
+    this._items.forEach((element) => {
+      total += element.price;
+    });
+    return Math.ceil(total);
+  }
+
+  amountWithDiscount(discount: number): number {
+    // итог стоимость с учетом скидки
+    let total = 0;
+    this._items.forEach((element) => {
+      total += element.price;
+    });
+    return Math.ceil(total - total * discount);
+  }
+
+  deleteItem(id: number): Buyable[] {
+    //удаляет товар из корзины по id
+    let index = this._items.findIndex((el) => el.id === id);
+    this._items.splice(index, 1);
+    return this._items;
+  }
+}
+
+const movie = new Movie(
+  14,
+  "Мастер и Маргарита",
+  2023,
+  "Россия",
+  "Дай волю фантазии",
+  "Фантастика",
+  180,
+  900
+);
+const movie1 = new Movie(
+  15,
+  "Летучий корабль",
+  2024,
+  "Россия",
+  "Окунись в детство",
+  "Фэнтези",
+  200,
+  800
+);
+const cart = new Cart();
+cart.add(movie);
+cart.add(movie1);
+cart.totalAmount();
+cart.amountWithDiscount(0.01);
+cart.deleteItem(14);
